60402091ae781e797ac4aa99091e938751b41364
I-Protecc <ryanbaumeister@googlemail.com>
2025-01-17 17:35:49 +0100

    feat: paintcomponent

---
+++ src/main/java/com/cozystudios/fofrathzindustry/GamePanel.java
@@ -0,0 +1,15 @@
+package com.cozystudios.fofrathzindustry;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel
+{
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        //g2.drawString("Test",100,100); //Just for testing purposes
+    }
+}
--- src/main/java/com/cozystudios/fofrathzindustry/Main.java
+++ src/main/java/com/cozystudios/fofrathzindustry/Main.java
@@ -6,13 +6,15 @@ public class Main
 {
     public static void main(String[] args)
     {
+        GamePanel gamePanel = new GamePanel();
         JFrame gameWindow = new JFrame();
+
         gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameWindow.setSize(1920,1080);  //final size / resize-ability is up to debate
         gameWindow.setTitle("FofrathzIndustry");
         gameWindow.setResizable(false); // Not resizeable for now, until we know how to stop it from stretching things
         gameWindow.setLocationRelativeTo(null);
         gameWindow.setVisible(true);
-
+        gameWindow.add(gamePanel);
     }
 }
