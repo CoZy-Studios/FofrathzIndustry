8629719b5569c00b75f5478d202073df46cf59ab
SzyZu <skwarek116@gmail.com>
2025-01-17 19:03:14 +0100

    feat: grid to coordinates calculator and vice versa

--- src/main/java/com/cozystudios/fofrathzindustry/Grid.java
+++ src/main/java/com/cozystudios/fofrathzindustry/Grid.java
@@ -3,4 +3,12 @@ package com.cozystudios.fofrathzindustry;
 public class Grid
 {
     private Tile[][] LocalGrid;
+
+    public static int GridToCoord(int tileInt){
+        return (tileInt * 32) - 32;
+    }
+
+    public static int CoordToGrid(int coordinate){
+        return (int)Math.floor(coordinate / 32) + 1;
+    }
 }
