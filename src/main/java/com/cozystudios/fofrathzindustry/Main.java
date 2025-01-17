b0fb619af979e6e36955d18b75e3a3f6ff8c058b
I-Protecc <ryanbaumeister@googlemail.com>
2025-01-17 16:40:18 +0100

    feat: Window base in main (First commit is mine >:D)

--- src/main/java/com/cozystudios/fofrathzindustry/Main.java
+++ src/main/java/com/cozystudios/fofrathzindustry/Main.java
@@ -1,7 +1,18 @@
 package com.cozystudios.fofrathzindustry;
+import java.awt.*;
+import javax.swing.*;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
+        JFrame gameWindow = new JFrame();
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameWindow.setSize(1920,1080);  //final size / resize-ability is up to debate
+        gameWindow.setTitle("FofrathzIndustry");
+        gameWindow.setResizable(false); // Not resizeable for now, until we know how to stop it from stretching things
+        gameWindow.setLocationRelativeTo(null);
+        gameWindow.setVisible(true);
 
     }
 }
