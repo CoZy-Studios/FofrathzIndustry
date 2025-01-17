81aeeb87ffad7c1933bbeec89d577e57e8a9d3ec
SzyZu <skwarek116@gmail.com>
2025-01-17 20:12:04 +0100

    chore: trying to draw underneath grid

--- src/main/java/com/cozystudios/fofrathzindustry/GamePanel.java
+++ src/main/java/com/cozystudios/fofrathzindustry/GamePanel.java
@@ -17,22 +17,6 @@ public class GamePanel extends JPanel
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
 
-        //g2.drawString("Test",100,100); //Just for testing purposes
-
-        // Draw horizontal lines
-        /*for (int i = 0; i <= rows; i++) {
-            int y = i * cellSize;
-            System.out.println("y: " + y);
-            g2.drawLine(0, y, width, y);
-        }
-
-        // Draw vertical lines
-        for (int i = 0; i <= columns; i++) {
-            int x = i * cellSize;
-            System.out.println("x: "+ x);
-            g2.drawLine(x, 0, x, height);
-        }*/
-
         grid.drawGrid(g2);
     }
 }
--- src/main/java/com/cozystudios/fofrathzindustry/Grid.java
+++ src/main/java/com/cozystudios/fofrathzindustry/Grid.java
@@ -6,26 +6,39 @@ public class Grid
 {
     int columns =  16;
     int rows = 16;
-    int cellSize = 34;
+    static int cellSize = 34;
     int width = columns * cellSize;
     int height = rows * cellSize;
     private Tile[][] LocalGrid;
 
     public static int GridToCoord(int tileInt){
-        return (tileInt * 32) - 32;
+        return (tileInt * cellSize) - cellSize;
     }
 
     public static int CoordToGrid(int coordinate){
-        return (int)Math.floor(coordinate / 32) + 1;
+        return (int)Math.floor(coordinate / cellSize) + 1;
     }
 
     public void drawGrid(Graphics2D g2)
     {
+        //populateGrid(g2);
         for(int i= 0; i <= rows; i++)
         {
             for(int j = 0; j <= columns; j++)
             {
                 g2.drawRect(j * cellSize, i * cellSize, cellSize, cellSize);
+            }
+        }
+    }
+
+    public void populateGrid(Graphics2D g2)
+    {
+        for(int i= 0; i <= rows; i++)
+        {
+            for(int j = 0; j <= columns; j++)
+            {
+                g2.setColor(Color.GREEN);
+                g2.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
             }
         }
     }
