a9cda44e7905eab00a6f6adf1d848aac3193a74f
I-Protecc <ryanbaumeister@googlemail.com>
2025-01-19 13:37:03 +0100

    fix: tiles save their color and only get drawn in that color, no more re randomizing

--- src/main/java/com/cozystudios/fofrathzindustry/Tile.java
+++ src/main/java/com/cozystudios/fofrathzindustry/Tile.java
@@ -7,25 +7,29 @@ import static com.cozystudios.fofrathzindustry.ColorHelper.*;
 public class Tile
 {
     private TileType tileType;
+    private Color tileColor = Color.white;
     int ID;
 
     public void initialize(TileType type, Graphics2D g2, int gridPositionX, int gridPositionY)
     {
         tileType = type;
+        if(tileColor == Color.white)
+        {
+            switch(tileType)
+            {
+                case Grass -> tileColor = grassRandomizer();
+                case Coal -> tileColor = coalRandomizer();
+                case Copper -> tileColor = copperRandomizer();
+                default -> g2.setColor(Color.WHITE);
+            }
+        }
         System.out.println("tile initialized, I am " + tileType);
         drawTile(g2 ,gridPositionX ,gridPositionY);
     }
 
     protected void drawTile(Graphics2D g2, int positionX, int positionY)
     {
-
-        switch(tileType)
-        {
-            case Grass -> g2.setColor(grassRandomizer());
-            case Coal -> g2.setColor(coalRandomizer());
-            case Copper -> g2.setColor(copperRandomizer());
-            default -> g2.setColor(Color.WHITE);
-        }
+        g2.setColor(tileColor);
         g2.fillRect((positionX - 1) * Grid.cellSize + 1, (positionY - 1) * Grid.cellSize + 1, Grid.cellSize - 1, Grid.cellSize - 1);
     }
 }
