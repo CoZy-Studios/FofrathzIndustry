f8addac602ae72f7060871ab747283df71193ead
SzyZu <skwarek116@gmail.com>
2025-01-22 16:10:39 +0100

    chore: working on ui panel

---
+++ src/main/java/com/cozystudios/fofrathzindustry/UiPanel.java
@@ -0,0 +1,18 @@
+﻿package com.cozystudios.fofrathzindustry;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UiPanel extends JPanel {
+
+    public UiPanel(){
+        JButton testButton = new JButton("Press for free cookies");
+        testButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                testButton.setText("blegh");
+            }
+        });
+    }
+}
