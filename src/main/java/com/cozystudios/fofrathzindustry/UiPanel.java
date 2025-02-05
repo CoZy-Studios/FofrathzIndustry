a43d83442ffb37cbc37f88fc63c47c041817706e
SzyZu <skwarek116@gmail.com>
2025-02-05 16:03:29 +0100

    chore: layout magic in the works

--- src/main/java/com/cozystudios/fofrathzindustry/UiPanel.java
+++ src/main/java/com/cozystudios/fofrathzindustry/UiPanel.java
@@ -1,13 +1,22 @@
 package com.cozystudios.fofrathzindustry;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class UiPanel extends JPanel {
 
     public UiPanel(){
-        JButton testButton = new JButton("Press for free cookies");
+        GridLayout layout = new GridLayout(3, 2, 0, 0);
+        this.setLayout(layout);
+
+        JLabel buildingMenuLabel = new JLabel("Building Menu");
+        JLabel buildLabel = new JLabel("Build");
+        JPanel emptyPanel = new JPanel();
+
+        JButton testButton = new JButton("Press");
+        JButton buildButton = new JButton("Build");
         testButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -15,6 +24,11 @@ public class UiPanel extends JPanel {
             }
         });
 
+        this.add(buildingMenuLabel);
+        this.add(emptyPanel);
+        this.add(buildLabel);
+        this.add(buildButton);
+        this.add(emptyPanel);
         this.add(testButton);
     }
 }
