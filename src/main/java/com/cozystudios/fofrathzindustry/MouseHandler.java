0936b6141f855aeb7b32e699e9ec8d7e39e5df2c
SzyZu <skwarek116@gmail.com>
2025-02-19 15:35:56 +0100

    feat: MouseHandler

---
+++ src/main/java/com/cozystudios/fofrathzindustry/MouseHandler.java
@@ -0,0 +1,39 @@
+﻿package com.cozystudios.fofrathzindustry;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseHandler implements MouseListener {
+
+    public boolean mousePressed = false;
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        mousePressed = true;
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        mousePressed = false;
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    public static Point getMousePos(){
+        return MouseInfo.getPointerInfo().getLocation();
+    }
+}
